5bd59834b4ebd704c148694f19c06bf73f7187a9
juanjg97 <juanjosejg2711@gmail.com>
2023-03-23 13:42:42 -0600

    Se hace la conexión de los servicios con EurekaServer

--- ProyectoUno/rate-service/src/main/java/com/bosonit/rateservice/RateServiceApplication.java
+++ ProyectoUno/rate-service/src/main/java/com/bosonit/rateservice/RateServiceApplication.java
@@ -2,8 +2,10 @@ package com.bosonit.rateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RateServiceApplication {
 
 	public static void main(String[] args) {
