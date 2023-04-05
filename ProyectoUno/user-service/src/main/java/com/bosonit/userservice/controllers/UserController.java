a0b35ca6b23f6a2138f5382889e8685f2c1ffa0a
juanjg97 <juanjosejg2711@gmail.com>
2023-04-04 18:07:08 -0600

    Implementación de tolerancia de fallos con circuit braker y retry

--- ProyectoUno/user-service/src/main/java/com/bosonit/userservice/controllers/UserController.java
+++ ProyectoUno/user-service/src/main/java/com/bosonit/userservice/controllers/UserController.java
@@ -2,8 +2,12 @@ package com.bosonit.userservice.controllers;
 
 import com.bosonit.userservice.entities.User;
 import com.bosonit.userservice.service.UserService;
+import com.sun.jdi.LongValue;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
+@Slf4j
 public class UserController {
     private UserService userService;
 
@@ -24,12 +29,14 @@ public class UserController {
 
     @Operation(summary = "Get a user from database using restTemplate")
     @GetMapping("/rest-template/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserRT(@PathVariable String userId){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserWithRestTemplate(userId));
     }
 
     @Operation(summary = "Get a user from database using FeignClient")
     @GetMapping("/feign-client/{userId}")
+    @Retry(name = "ratingHotelService",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserFC(@PathVariable String userId){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserWithFeignClient(userId));
     }
@@ -38,6 +45,16 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> usersList(){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    }
+
+    public ResponseEntity<User> ratingHotelFallback(String usuarioId,Exception exception){
+        log.info("El respaldo se ejecuta porque el servicio esta inactivo : ",exception.getMessage());
+        User usuario = User.builder()
+                .email("root1@gmail.com")
+                .name("root")
+                .information("Este usuario se crea por defecto cuando un servicio se cae")
+                .build();
+        return new ResponseEntity<>(usuario,HttpStatus.OK);
     }
 
 }
